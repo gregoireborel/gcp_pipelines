3024a37d927870a50f89f26e8d66cff294851406
Gregoire Borel <gregoire.borel@neoxia.com>
2024-07-14 19:28:29 +0200

    Amend pipeline

--- dataflow-pipelines/scripts/build_with_kaniko.sh
+++ dataflow-pipelines/scripts/build_with_kaniko.sh
@@ -2,24 +2,27 @@
 # Exit when any command fails
 set -euo pipefail
 
-echo $1
-for folder in $1; do
-    echo "Building pipeline in folder: $folder"
-    cd $folder
+if [ -s /workspace/changed_folders ]; then
+    while read folder; do
+        echo "Building pipeline in folder: $folder"
+        # cd ../$folder
 
-    IMAGE_NAME="${$folder,,}"
-    echo "### Building Dataflow Docker image with Kaniko ###"
+        # echo "### Building Dataflow Docker image with Kaniko ###"
+        # IMAGE_NAME="${$folder,,}"
 
-    docker build -t ${_LOCATION}-docker.pkg.dev/$PROJECT_ID/${_DOCKER_REPO_NAME}/$IMAGE_NAME:latest '.'
+        # docker build -t ${_LOCATION}-docker.pkg.dev/$PROJECT_ID/${_DOCKER_REPO_NAME}/$IMAGE_NAME:latest '.'
 
-    # /kaniko/executor \
-    # --use-new-run \
-    # --compressed-caching="false" \
-    # --single-snapshot \
-    # --context . \
-    # --dockerfile ./$folder/Dockerfile \
-    # --destination "$LOCATION-docker.pkg.dev/$PROJECT_ID/$_DOCKER_REPO_NAME/$IMAGE_NAME:latest"
+        # /kaniko/executor \
+        # --use-new-run \
+        # --compressed-caching="false" \
+        # --single-snapshot \
+        # --context . \
+        # --dockerfile ./$folder/Dockerfile \
+        # --destination "$LOCATION-docker.pkg.dev/$PROJECT_ID/$_DOCKER_REPO_NAME/$IMAGE_NAME:latest"
 
-    # Go back to the root directory before processing the next folder
-    cd - > /dev/null
-done
+        # # Go back to the root directory before processing the next folder
+        # cd - > /dev/null
+    done </workspace/changed_folders
+else
+    echo "No relevant changes detected."
+fi
