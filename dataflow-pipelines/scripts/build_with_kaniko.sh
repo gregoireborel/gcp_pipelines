cfed278b0acd4629f5d80b1d9ae08f5c44ecdbf4
Gregoire Borel <gregoire.borel@neoxia.com>
2024-07-15 10:49:50 +0200

    Test env field

--- dataflow-pipelines/scripts/build_with_kaniko.sh
+++ dataflow-pipelines/scripts/build_with_kaniko.sh
@@ -2,22 +2,18 @@
 # Exit when any command fails
 set -euo pipefail
 
-_LOCATION=$1
-_PROJECT_ID=$2
-_DOCKER_REPO_NAME=$3
-
 if [ -s /workspace/changed_folders ]; then
     while IFS="" read -r folder || [ -n "$folder" ]
     do
         echo "##### Building pipeline in folder: $folder "#####
         cd ../$folder
 
-        _IMAGE_NAME=$(echo "$folder" | tr '[:upper:]' '[:lower:]')
-        echo "##### Building Dataflow Docker image $_IMAGE_NAME with Kaniko"#####
+        IMAGE_NAME=$(echo "$folder" | tr '[:upper:]' '[:lower:]')
+        echo "##### Building Dataflow Docker image $IMAGE_NAME with Kaniko"#####
 
         /kaniko/executor \
         --cache=true \
-        --destination "$_LOCATION-docker.pkg.dev/$_PROJECT_ID/$_DOCKER_REPO_NAME/$_IMAGE_NAME:latest"
+        --destination "$LOCATION-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/$IMAGE_NAME:latest"
 
         # Go back to the root directory before processing the next folder
         cd - > /dev/null
--- dataflow-pipelines/scripts/create_flex_template_spec_file_gcs.sh
+++ dataflow-pipelines/scripts/create_flex_template_spec_file_gcs.sh
@@ -2,25 +2,19 @@
 # Exit when any command fails
 set -euo pipefail
 
-_LOCATION=$1
-_PROJECT_ID=$2
-_DOCKER_REPO_NAME=$3
-_SDK_LANGUAGE=$4
-_DATAFLOW_BUCKET=$5
-_CI_SERVICE_NAME=$6
-
 if [ -s /workspace/changed_folders ]; then
     while IFS="" read -r folder || [ -n "$folder" ]
     do
-        _IMAGE_NAME=$(echo "$folder" | tr '[:upper:]' '[:lower:]')
+        IMAGE_NAME=$(echo "$folder" | tr '[:upper:]' '[:lower:]')
 
         echo "##### Creating JSON specificaton file for $folder Docker image #####"
 
-        gcloud dataflow flex-template build "gs://$_DATAFLOW_BUCKET/$folder-$_CI_SERVICE_NAME.json" \
-        --image "$_LOCATION-docker.pkg.dev/$_PROJECT_ID/$_DOCKER_REPO_NAME/$_IMAGE_NAME:latest" \
-        --sdk-language "$_SDK_LANGUAGE"
+        gcloud dataflow flex-template build "gs://$DATAFLOW_BUCKET/$folder-$CI_SERVICE_NAME.json" \
+        --image "$LOCATION-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/$IMAGE_NAME:latest" \
+        --sdk-language "$SDK_LANGUAGE"
         #--metadata-file "$METADATA_FILE"
     done < /workspace/changed_folders
+    rm /workspace/changed_folders
 else
     echo "No changes to pipeline code detected"
 fi
