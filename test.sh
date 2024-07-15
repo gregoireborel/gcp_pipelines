cf46b8777cbaf034047d9ec958e440e645191dfd
Gregoire Borel <gregoire.borel@neoxia.com>
2024-07-15 16:34:07 +0200

    No pipeline change

--- test.sh
+++
@@ -1,5 +0,0 @@
- _CHANGED_FOLDERS="$(git diff --dirstat=files,0 HEAD^ HEAD -- dataflow-pipelines/Flex* | sed 's/^[ 0-9.]\+% //g' | cut -d'/' -f2 | uniq)"
- if [ "$_CHANGED_FOLDERS" ]; then
-     echo "CHANGES"
-     echo "$_CHANGED_FOLDERS" > hey
- fi
