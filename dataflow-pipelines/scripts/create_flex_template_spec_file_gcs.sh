7cc35e3dd8bede82c0c25753a4d6d83f21346634
Gregoire Borel <gregoire.borel@neoxia.com>
2024-07-15 11:02:15 +0200

    Modifications without pipeline code change

--- dataflow-pipelines/scripts/create_flex_template_spec_file_gcs.sh
+++ dataflow-pipelines/scripts/create_flex_template_spec_file_gcs.sh
@@ -6,8 +6,7 @@ if [ -s /workspace/changed_folders ]; then
     while IFS="" read -r folder || [ -n "$folder" ]
     do
         IMAGE_NAME=$(echo "$folder" | tr '[:upper:]' '[:lower:]')
-
-        echo "##### Creating JSON specificaton file for $folder Docker image #####"
+        echo "##### Building JSON specificaton file for $folder Docker image #####"
 
         gcloud dataflow flex-template build "gs://$DATAFLOW_BUCKET/$folder-$CI_SERVICE_NAME.json" \
         --image "$LOCATION-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/$IMAGE_NAME:latest" \
