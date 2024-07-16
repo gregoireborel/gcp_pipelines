56cc01d0c09e353bae343cca648b12bafcb6ea73
Gregoire Borel <gregoire.borel@neoxia.com>
2024-07-17 00:56:52 +0200

    Move run_locally.sh to pipeline folder

---
+++ dataflow-pipelines/Flex-PubSub-Dataflow-BiqQuery/run_locally.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -euo pipefail
+
+PROJECT=gborel-sample-project
+LOCATION=europe-west1
+# No uppercase letters
+PIPELINE=dataflow-test-pipeline
+BUCKET="gb-dataflow-flex-templates"
+
+python -m main \
+  --project=$PROJECT \
+  --job_name="local-$PIPELINE-`date +%Y%m%d-%H%M%S`" \
+  --runner=DirectRunner \
+  --region $LOCATION \
+  --temp-location gs://$BUCKET/temp \
+  --staging-location gs://$BUCKET/staging \
+  --mode=batch \
+  --bucket_name=les-chiens-bananes-media \
+  --destination_project_id=gborel-sample-project \
+  --dataset=gborel-sample-dataset \
+  --dataset_errors=gborel-sample-dataset-errors \
+  --table=BANANE
