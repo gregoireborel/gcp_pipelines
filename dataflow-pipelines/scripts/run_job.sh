ac2d06564ca6aec046acc3938d45a6c76e74291a
Gregoire Borel <gregoire.borel@neoxia.com>
2024-07-17 00:45:43 +0200

    Add MODE and ENV vars

--- dataflow-pipelines/scripts/run_job.sh
+++ dataflow-pipelines/scripts/run_job.sh
@@ -6,10 +6,10 @@ set -euo pipefail
 echo "##### Run the Dataflow Flex Template $PIPELINE_NAME pipeline #####"
 
 IMAGE_NAME=$(echo "$PIPELINE_NAME" | tr '[:upper:]' '[:lower:]')
-
+pwd
 gcloud dataflow flex-template run "$PIPELINE_NAME-$CI_SERVICE_NAME-$(date +%Y%m%d-%H%M%S)" \
   --template-file-gcs-location "gs://$DATAFLOW_BUCKET/$IMAGE_NAME-$CI_SERVICE_NAME.json" \
   --project="$PROJECT_ID" \
   --region="$LOCATION" \
   --temp-location="$DATAFLOW_BUCKET/temp" \
-  --staging-location="$DATAFLOW_BUCKET/staging" \ $(< "$FILE_PATH")
+  --staging-location="$DATAFLOW_BUCKET/staging" \ $(< "dataflow-pipelines/$PIPELINE_NAME/ci/$MODE/${ENV}_parameters.txt")
