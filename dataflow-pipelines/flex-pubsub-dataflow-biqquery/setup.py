ab30dfe9e61c9e1d75c1483d3b4fe0a39f6dd7ba
Gregoire Borel <grg.borel@gmail.com>
2024-07-18 15:26:54 +0200

    Amend Dockerfile, update beam version

--- dataflow-pipelines/flex-pubsub-dataflow-biqquery/setup.py
+++ dataflow-pipelines/flex-pubsub-dataflow-biqquery/setup.py
@@ -2,12 +2,12 @@
 import setuptools
 
 PACKAGE_VERSION = "1.0.0"
-PACKAGE_NAME = "flex-PubSub-Dataflow-BiqQuery-batch"
+PACKAGE_NAME = "flex-pubsub-dataflow-bigquery-batch"
 
 # List here only libraries your SDK won't work with! Here some examples
 # Now if we install the package normally : pip install -e .
 install_requires = [
-    "apache-beam[gcp]==2.54.0",
+    "apache-beam[gcp]==2.57.0",
     "google-api-core==2.15.0",
     "google-cloud-pubsub==2.19.0",
     "google-auth==2.26.2",
@@ -18,7 +18,7 @@ setuptools.setup(
     name=PACKAGE_NAME,
     packages=setuptools.find_packages(),
     version=PACKAGE_VERSION,
-    description="flex-PubSub-Dataflow-BiqQuery-batch processing package.",
+    description=f"{PACKAGE_NAME} processing package.",
     python_requires=">=3.11",
     install_requires=install_requires,
 )
