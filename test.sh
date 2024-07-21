4a198c8ab3bb9d564042513dfc4396cfa04e9d56
Gregoire Borel <grg.borel@gmail.com>
2024-07-21 20:02:33 +0200

    Remove test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-BRANCH_NAME=feature/refacto
-CLEAN_BRANCH_NAME=$(echo "$BRANCH_NAME" | tr '[:upper:]' '[:lower:]')
-CLEAN_BRANCH_NAME="${CLEAN_BRANCH_NAME////-}"
-echo $CLEAN_BRANCH_NAME
